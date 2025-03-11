10217700a50c76055a8a9aa84db001398b1cd51a
Haris Sabanovic <hasa2303@student.miun.se>
2025-03-11 15:16:38 +0100

    Skapade ett formulär

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
+import './css/nav.css'
 import router from './routing.tsx'
 
 createRoot(document.getElementById('root')!).render(
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,16 @@
+import '../css/loginForm.css';
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className="login-container">
+      <div className="book-icon">📚</div>
+      <h1 className="login-header">Bokklubb Inloggning</h1>
+      <form className="login-form">
+          <input type="text" placeholder="Användarnamn" required />
+          <input type="password" placeholder="Lösenord" required />
+          <button type="submit" className="login-btn">Logga in</button>
+      </form>
+    </div>
   )
 }
 
