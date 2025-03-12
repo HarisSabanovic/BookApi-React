9458524e60df1f0f8fa459b40ed2d1e73b1f017f
Haris Sabanovic <hasa2303@student.miun.se>
2025-03-12 13:24:58 +0100

    skapar states

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import '../css/loginForm.css';
 
 const LoginPage = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+
+  const submitHandler = async(event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  }
+  
   return (
     <div className="login-container">
       <div className="book-icon">📚</div>
       <h1 className="login-header">Bokklubb Inloggning</h1>
-      <form className="login-form">
-          <input type="text" placeholder="Användarnamn" required />
-          <input type="password" placeholder="Lösenord" required />
+      <form className="login-form" onSubmit={submitHandler}>
+          <input type="text" placeholder="Användarnamn" required value={username} onChange={(event) => setUsername(event.target.value)} />
+          <input type="password" placeholder="Lösenord" required value={password} onChange={(event) => setPassword(event.target.value)}/>
           <button type="submit" className="login-btn">Logga in</button>
       </form>
     </div>
