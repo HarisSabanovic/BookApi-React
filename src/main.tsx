3b9bd9446a7742cc25afa980e54ad26fd56e1966
Haris Sabanovic <hasa2303@student.miun.se>
2025-03-13 12:59:40 +0100

    importerar authprovider

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import './css/nav.css'
 import router from './routing.tsx'
+import { AuthProvider } from './context/authContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <AuthProvider>
     <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
