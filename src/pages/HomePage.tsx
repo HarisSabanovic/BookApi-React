5a098a2a9418b111d005c55318aa069e7f49de27
Haris Sabanovic <hasa2303@student.miun.se>
2025-03-17 11:48:25 +0100

    Lade till sökfunktion som hämtar bokdata baserat på användarens sökfråga. Visar bokdetaljer och låter användaren välja en bok för mer information.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,32 @@
 import { useState } from "react"
+import BookCard from "../components/BookCard";
 
+interface Book {
+  id: string;
+  volumeInfo: {
+      title: string;
+      authors?: string[];
+      publishedDate?: string;
+      description?: string;
+      imageLinks?: {
+          thumbnail: string;
+      };
+  };
+}
 
 const HomePage = () => {
   const [query, setQuery] = useState("");
-  const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState<Book[]>([]);
+  const [selectedBook, setSelectedBook] = useState<Book | null>(null);
+  const apiKey = import.meta.env.VITE_API_KEY;
+
 
   const searchBooks = async () => {
     try {
-      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${process.env.REACT_APP_API_KEY}`);
-      const data = response.json();
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`);
+      const data = await response.json();
       setBooks(data.items || []);
+      console.log(data.items);
     } catch(error) {
       console.error("Fel vid hämtning av böcker")
     }
@@ -17,8 +34,31 @@ const HomePage = () => {
   return (
     <div>
         <h1>Books Search</h1>
-        <input type="text" value={query} onChange={(event) => setQuery(event.target.value)} placeholder="Sök efter böcker..."/>
-        <button onClick={searchBooks}>Sök</button>
+              <input 
+                  type="text" 
+                  value={query} 
+                  onChange={(e) => setQuery(e.target.value)} 
+                  placeholder="Sök efter böcker..."
+              />
+              <button onClick={searchBooks}>Sök</button>
+
+
+              <div>
+                {books.map((book) => (
+                    <BookCard 
+                        key={book.id}
+                        book={book} 
+                        onSelect={(selected) => setSelectedBook(selected)} 
+                    />
+                ))}
+            </div>
+            
+            {selectedBook && (
+                <div>
+                    <h2>{selectedBook.volumeInfo.title}</h2>
+                    <p>{selectedBook.volumeInfo.description || "Ingen beskrivning tillgänglig."}</p>
+                </div>
+            )}
     </div>
   )
 }
